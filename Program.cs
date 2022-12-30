590f5dab1aa6ee3d1fdf981e7cc52539c2240e04
Khaledoraby <121660707+Khaledoraby@users.noreply.github.com>
2022-12-30 11:30:23 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace perfect_number
+{
+    internal class Program
+    {
+        static bool Isperfect(int number)
+        {
+
+            int sum = 0;    
+            for (int i=1;i<number;i++)
+            {
+                if (number%i == 0) sum=sum+1;  
+            }
+
+            if (sum == number) return true;
+          
+            return false;
+
+        }
+
+
+        static void Main(string[]args)
+        {
+
+            int x,y ;
+            Console.WriteLine("enter the frist num ");
+                x = int.Parse(Console.ReadLine());
+            Console.WriteLine("enter the second num");
+            y = int.Parse(Console.ReadLine());
+            Console.WriteLine("\n");
+
+            for( int i= x ;i<= y ; i++)
+            {
+                if (Isperfect(i)) Console.WriteLine("{0}, "+i);
+
+                
+            }
+
+
+
+            Console.ReadKey();  
+
+        }
+
+
+
+
+
+
+
+
+    }
+}
